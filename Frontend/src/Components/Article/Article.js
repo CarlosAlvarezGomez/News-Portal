58a8f4c0d8a891626fa4b8d975378815a1ca0633
ca446 <ca446@cornell.edu>
2022-02-05 18:39:55 -0500

    Made some changes to classifier/articles
    Make changes to classifier, simplified articles code, and made articles use filler values instead of calling API

--- Frontend/src/Components/Article/Article.js
+++ Frontend/src/Components/Article/Article.js
@@ -12,7 +12,7 @@ export function Article(art, index, columnName) {
 
   // Checks if an article has an image or not, and returns the html code with
   // and without the <img> respectively
-  if (art.Image !== "") {
+  // if (art.Image !== "") {
     return (
       //<a href={'http://localhost:3000/article/' + art.ID}>
       <a href={art.Link}>
@@ -23,28 +23,29 @@ export function Article(art, index, columnName) {
             <h6 className="text">Updated {art.UpdateTime.substring(0,20)}</h6>
             <h4 className="text">{art.SubHeadline}</h4>
           </div>
+          {art.Image !== "" && 
           <div className="image">
             <img src={art.Image} alt='Not Found' />
-          </div>
+          </div>}
         </div>
       </a>
     );
     
-  } else {
-    return (
-      //<a href={'http://localhost:3000/article/' + art.ID}>
-      <a href={art.Link}>
-        <div className="article">
-          <div className="textGroup2">
-            <h3 className="text">{art.Headline}</h3>
-            <h6 className="text">By {art.Author}</h6>
-            <h6 className="text">Updated {art.UpdateTime.substring(0,20)}</h6>
-            <h4 className="text">{art.SubHeadline}</h4>
-          </div>
-        </div>
-      </a>
-    );
-  }
+  // } else {
+  //   return (
+  //     //<a href={'http://localhost:3000/article/' + art.ID}>
+  //     <a href={art.Link}>
+  //       <div className="article">
+  //         <div className="textGroup2">
+  //           <h3 className="text">{art.Headline}</h3>
+  //           <h6 className="text">By {art.Author}</h6>
+  //           <h6 className="text">Updated {art.UpdateTime.substring(0,20)}</h6>
+  //           <h4 className="text">{art.SubHeadline}</h4>
+  //         </div>
+  //       </div>
+  //     </a>
+  //   );
+  // }
 
   
 }
--- Frontend/src/Components/ArticleList/useGetArticles.js
+++ Frontend/src/Components/ArticleList/useGetArticles.js
@@ -9,27 +9,48 @@ export function useGetArticles(category, pageNumber) {
 
   // This funciton is used whenever pageNumber changes
   useEffect(
+
+
     () =>  {
+      const output = Array(10).fill({
+        Link: "www.google.com",
+        Headline: "ARTICLE HEADLINE",
+        Author: "Someone",
+        SubHeadline: "Sub-headline for this article",
+        UpdateTime: "January 1, 2022, 12:00 AM"
+        })
       setLoading(true);
       setError(false);
-
-      // Requst next page of articles from api and append it to current list of
-      // articles
-      axios({
-        method: 'GET',
-        url:'http://localhost:8081/articles/',
-        params: {category: category, requestNumber: pageNumber}
-      }).then(res => {
-
-        // Appends the new articles to the end of the current article list
-        setArticles([... articles, ...res.data])
-      }).catch( err => {
-        setError(true)
-      })
-
+      setArticles([... articles, ])
       setHasMore(true);
       setLoading(false);
   }, [pageNumber])
 
   return { loading, error, articles, hasMore };
 }
+
+// Old code
+//     () =>  {
+//       setLoading(true);
+//       setError(false);
+
+//       // Requst next page of articles from api and append it to current list of
+//       // articles
+//       axios({
+//         method: 'GET',
+//         url:'http://localhost:8081/articles/',
+//         params: {category: category, requestNumber: pageNumber}
+//       }).then(res => {
+
+//         // Appends the new articles to the end of the current article list
+//         setArticles([... articles, ...res.data])
+//       }).catch( err => {
+//         setError(true)
+//       })
+
+//       setHasMore(true);
+//       setLoading(false);
+//   }, [pageNumber])
+
+//   return { loading, error, articles, hasMore };
+// }
